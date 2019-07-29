fc49cc63d944c0290860b8440b6aee0fd2135dd8
khoitd1997 <khoidinhtrinh@gmail.com>
2019-07-28 18:29:02 -0700

    added build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ${currDir}
+
+mkdir -p build
+cd build
+cmake ..
+cmake --build .
+cp ${currDir}/wav/sine_test.wav ${currDir}/build/bin
+
+cd ${currDir}/build/bin
+./ringer_exe
